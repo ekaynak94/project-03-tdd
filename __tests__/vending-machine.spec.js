9078308a86fd445adc7b30ec59a5dc65a8c2d167
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-26 16:25:28 -0700

    Add template code for tests

---
+++ __tests__/vending-machine.spec.js
@@ -0,0 +1,70 @@
+const VendingMachine = require("../src/vending-machine");
+
+describe('VendingMachine', () => {
+
+    // beforeEach(() => {
+        
+    // });
+
+    // describe('test1', () => {
+    //     it('should return result1',()=> {
+            
+    //     });
+    // });
+
+    // describe('test2', () => {
+    //     it('should return result2',()=> {
+            
+    //     });
+    // });
+
+    // describe('test3', () => {
+    //     it('should return result3',()=> {
+            
+    //     });
+    // });
+
+    // describe('test4', () => {
+    //     it('should return result4',()=> {
+            
+    //     });
+    // });
+
+    // describe('test5', () => {
+    //     it('should return result5',()=> {
+            
+    //     });
+    // });
+
+    // describe('test6', () => {
+    //     it('should return result6',()=> {
+            
+    //     });
+    // });
+    
+    // describe('test7', () => {
+    //     it('should return result7',()=> {
+            
+    //     });
+    // });
+
+    // describe('test8', () => {
+    //     it('should return result8',()=> {
+            
+    //     });
+    // });
+
+    // describe('test9', () => {
+    //     it('should return result9',()=> {
+            
+    //     });
+    // });
+
+    // describe('test10', () => {
+    //     it('should return result10',()=> {
+            
+    //     });
+    // });
+
+
+});
