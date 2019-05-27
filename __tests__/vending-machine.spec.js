01e22a62f89e1c3e69437a06afc167d967ae725e
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-26 17:52:42 -0700

    Add test for insert coin method

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -1,70 +1,27 @@
-const VendingMachine = require("../src/vending-machine");
+const VendingMachine = require("../src/vending-machine.js");
+initialState =require('../imports/initial-vending-machine.json');
 
 describe('VendingMachine', () => {
 
-    // beforeEach(() => {
+    beforeEach(() => {
+        const test = {};
+        test.sampleMachine = initialState;
+    });
+
+    describe('insertCoin()', () => {
         
-    // });
+        beforeEach(() => {
+            test.vendingMachine = new VendingMachine();
+        });
 
-    // describe('test1', () => {
-    //     it('should return result1',()=> {
-            
-    //     });
-    // });
+        describe('if inserted unknown currency', () => {
+            it('should return an error', () => {
+                expect(() => { test.vendingMachine.insertCoin("penny"); }).toThrow(new Error('currency not recognized'));
+            });
+        })
 
-    // describe('test2', () => {
-    //     it('should return result2',()=> {
-            
-    //     });
-    // });
+    });
 
-    // describe('test3', () => {
-    //     it('should return result3',()=> {
-            
-    //     });
-    // });
-
-    // describe('test4', () => {
-    //     it('should return result4',()=> {
-            
-    //     });
-    // });
-
-    // describe('test5', () => {
-    //     it('should return result5',()=> {
-            
-    //     });
-    // });
-
-    // describe('test6', () => {
-    //     it('should return result6',()=> {
-            
-    //     });
-    // });
-    
-    // describe('test7', () => {
-    //     it('should return result7',()=> {
-            
-    //     });
-    // });
-
-    // describe('test8', () => {
-    //     it('should return result8',()=> {
-            
-    //     });
-    // });
-
-    // describe('test9', () => {
-    //     it('should return result9',()=> {
-            
-    //     });
-    // });
-
-    // describe('test10', () => {
-    //     it('should return result10',()=> {
-            
-    //     });
-    // });
 
 
 });
