fec4c589f6e1a0d404937938d2e7ac5b49bf97ab
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-26 18:03:38 -0700

    Alter code to pass test

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -2,28 +2,28 @@ class VendingMachine {
     constructor() {
         this.inventory = {};
         this._coins = {
-            "nickels":{ 
-                "value":0.05 ,
-                 "number":0 },
-            "dimes":{ 
-                "value":0.10 , 
-                "number":0 },
-            "quarters":{
-                 "value":0.25 ,
-                 "number":0 },
-            "loonies":{
-                 "value":1 , 
-                 "number":0 },
-            "toonies":{ 
-                "value":2 ,
-                "number":0 }
+            nickels:{ 
+                value:0.05 ,
+                 number:0 },
+            dimes:{ 
+                value:0.10 , 
+                number:0 },
+            quarters:{
+                 value:0.25 ,
+                 number:0 },
+            loonies:{
+                 value:1 , 
+                 number:0 },
+            toonies:{ 
+                value:2 ,
+                number:0 }
         };
         this._userFunds = 0;
     }
 
     insertCoin(coin){
         if (!this._coins[coin]) throw new Error('currency not recognized');
-        this._coins[coin].value = this._coins[coin].value + 1;
+        this._coins[coin].number = this._coins[coin].number + 1;
     }
 }
 
