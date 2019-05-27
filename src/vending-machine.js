f177252edca6a6138ca2c9a5980b097fd65500b5
Emre Kaynak <emrekaynak94@gmail.com>
2019-05-26 17:53:11 -0700

    Implement insert coin to pass initial test

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,8 +1,29 @@
 class VendingMachine {
-    constuctor() {
-        this._inventory = {};
-        this._coins = {};
+    constructor() {
+        this.inventory = {};
+        this._coins = {
+            "nickels":{ 
+                "value":0.05 ,
+                 "number":0 },
+            "dimes":{ 
+                "value":0.10 , 
+                "number":0 },
+            "quarters":{
+                 "value":0.25 ,
+                 "number":0 },
+            "loonies":{
+                 "value":1 , 
+                 "number":0 },
+            "toonies":{ 
+                "value":2 ,
+                "number":0 }
+        };
         this._userFunds = 0;
+    }
+
+    insertCoin(coin){
+        if (!this._coins[coin]) throw new Error('currency not recognized');
+        this._coins[coin].value = this._coins[coin].value + 1;
     }
 }
 
